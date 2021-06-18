8976466053bc18a19ff757aaa860f9c4907f75e5
Boris <rozumniy.boris.work@gmail.com>
2021-06-18 17:42:40 +0300

    created the isChords to the RenderSong

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { RenderSong } from "./components/RenderSong";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   const [songContent, setSongContent] =
     useState("");
 
-  const handleChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.FormEvent<HTMLTextAreaElement>
+  ) => {
     const { value } = e.currentTarget;
     setSongContent(value);
   };
@@ -22,7 +24,9 @@ function App() {
         />
         <button>render</button>
       </header>
-      <RenderSong />
+      <RenderSong
+        songContent={songContent}
+      />
     </div>
   );
 }
--- src/components/RenderSong.tsx
+++ src/components/RenderSong.tsx
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from "react";
 
 type Props = {
+  songContent: string;
   content?: (val: any) => void;
 };
 
-export const RenderSong: React.FC<Props> = ({ content }) => <h1>RenderSong</h1>;
+export const RenderSong: React.FC<Props> =
+  ({ songContent }) => {
+    const isChords = (row: string) => {
+      const CHORDS_LIST = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+      ];
+      const ALTERATION_SIGNS = [
+        "#",
+        "b",
+      ];
+      const TONE_LAD = [
+        "m",
+        "maj",
+        "dim",
+        "+",
+        "sus",
+      ];
+      let generated_list: string[] = [];
+      CHORDS_LIST.forEach((c) => {
+        generated_list.push(c);
+        ALTERATION_SIGNS.forEach((a) =>
+          generated_list.push(c + a)
+        );
+      });
+      const chordsFromRow = row
+        ?.split(" ")
+        .filter((rowItem) => rowItem);
+
+      const res = chordsFromRow?.every(
+        (chordInput) => {
+          return generated_list.some(
+            (chord) => {
+              return chordInput.includes(
+                chord
+              );
+            }
+          );
+        }
+      );
+
+      return res;
+    };
+
+    const [textRows, setTextRows] =
+      useState([] as string[]);
+    useEffect(() => {
+      setTextRows(
+        songContent.split("\n")
+      );
+      isChords(textRows[1]);
+    }, [songContent]);
+
+    return (
+      <>
+        {textRows.map((row) => {
+          return isChords(row) ? (
+            <h3 key={row}>{row}</h3>
+          ) : (
+            <p>{row}</p>
+          );
+        })}
+      </>
+    );
+  };
