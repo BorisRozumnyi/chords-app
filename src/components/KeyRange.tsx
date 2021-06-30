590e5d4d3a48e00c504cd4c1527e2c958fe35132
Boris <rozumniy.boris.work@gmail.com>
2021-06-30 15:46:19 +0300

    created the KeyRange

---
+++ src/components/KeyRange.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { ChordButtom } from "../styles";
+
+const CHORDS_LIST = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "|",
+];
+const ALTERATION_SIGNS = ["#", "b"];
+let generated_list: string[] = [];
+CHORDS_LIST.forEach((c) => {
+  generated_list.push(c);
+  ALTERATION_SIGNS.forEach((a) =>
+    generated_list.push(c + a)
+  );
+});
+
+type Props = {
+  prop?: (val: any) => void;
+};
+
+export const KeyRange: React.FC<Props> =
+  ({ prop }) => (
+    <>
+      <h1>KeyRange</h1>
+      {generated_list.map((item) => (
+        <ChordButtom key={item}>
+          {item}
+        </ChordButtom>
+      ))}
+    </>
+  );
