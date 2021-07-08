522bf8021dc60a679801fa560d1863617bd4f338
Boris <rozumniy.boris.work@gmail.com>
2021-07-08 04:41:45 +0300

    added the fifthCircle method in the Tonality class

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,55 @@
-import React, {
-  useEffect,
-  useState,
-} from "react";
-import { RenderSong } from "./components/RenderSong";
-import { KeyRange } from "./components/KeyRange";
-import {
-  AppWrapper,
-  Container,
-  EnteringContent,
-} from "./styles";
-import { isChords } from "./utils";
-import { ChordContext } from "./utils/context";
+import React, { useEffect, useState } from 'react';
+import { RenderSong } from './components/RenderSong';
+import { KeyRange } from './components/KeyRange';
+import { AppWrapper, Container, EnteringContent } from './styles';
+import { isChords, Tonality } from './utils';
+import { ChordContext } from './utils/context';
 
 export const App = () => {
-  const [songContent, setSongContent] =
-    useState("");
+  const [songContent, setSongContent] = useState('');
 
-  const [editMode, setEditMode] =
-    useState(false);
+  const [editMode, setEditMode] = useState(false);
 
-  const [
-    originTonality,
-    setOriginTonality,
-  ] = useState<string>("");
+  const [originTonality, setOriginTonality] = useState<string>('');
 
-  const [
-    currentTonality,
-    setCurrentTonality,
-  ] = useState<string>("");
+  const [currentTonality, setCurrentTonality] = useState<string>('');
 
   useEffect(() => {
-    const tonalityInParentheses =
-      songContent
-        .split("\n")[0]
-        .match(/\([A-H](#?|b?)(m?)\)/);
+    const tonalityInParentheses = songContent
+      .split('\n')[0]
+      .match(/\([A-H](#?|b?)(m?)\)/);
 
     const firstChord = songContent
-      .split("\n")
+      .split('\n')
       .find((row) => isChords(row))
-      ?.split(" ")[0];
+      ?.split(' ')[0];
 
     if (tonalityInParentheses) {
-      const tonalityWithoutParentheses =
-        tonalityInParentheses[0]
-          .replace("(", "")
-          .replace(")", "");
-      setOriginTonality(
-        tonalityWithoutParentheses
-      );
-      setCurrentTonality(
-        tonalityWithoutParentheses
-      );
+      const tonalityWithoutParentheses = tonalityInParentheses[0]
+        .replace('(', '')
+        .replace(')', '');
+      setOriginTonality(tonalityWithoutParentheses);
+      setCurrentTonality(tonalityWithoutParentheses);
     }
 
-    if (
-      firstChord &&
-      !tonalityInParentheses
-    ) {
+    if (firstChord && !tonalityInParentheses) {
       setOriginTonality(firstChord);
       setCurrentTonality(firstChord);
     }
 
-    if (
-      firstChord &&
-      !tonalityInParentheses &&
-      !currentTonality
-    ) {
+    if (firstChord && !tonalityInParentheses && !currentTonality) {
       setCurrentTonality(firstChord);
     }
   }, [songContent]);
 
+  const testTonality = new Tonality().getTonalitySteps(currentTonality);
+    console.log(
+      '\n testTonality---', testTonality,
+      '\n fifthCircle()---', new Tonality().fifthCircle(),
+    );
+
   const handleChangeEnteringContent = (
-    e: React.FormEvent<HTMLTextAreaElement>
+    e: React.FormEvent<HTMLTextAreaElement>,
   ) => {
     const { value } = e.currentTarget;
     setSongContent(value);
@@ -92,24 +70,14 @@ export const App = () => {
           {editMode ? (
             <EnteringContent
               value={songContent}
-              onChange={
-                handleChangeEnteringContent
-              }
+              onChange={handleChangeEnteringContent}
               placeholder="Enter text with chords here"
-              onBlur={() =>
-                setEditMode(false)
-              }
+              onBlur={() => setEditMode(false)}
               autoFocus
-              rows={
-                songContent.split("\n")
-                  .length
-              }
+              rows={songContent.split('\n').length}
             />
           ) : (
-            <RenderSong
-              songContent={songContent}
-              setEditMode={setEditMode}
-            />
+            <RenderSong songContent={songContent} setEditMode={setEditMode} />
           )}
         </Container>
       </AppWrapper>
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,225 +1,164 @@
 const T = [
   {
-    natural: "C",
-    enharmonicSharp: "H#",
-    enharmonicFlat: "",
+    natural: 'C',
+    enharmonicSharp: 'H#',
+    enharmonicFlat: '',
   },
   {
-    natural: "C#",
-    enharmonicSharp: "",
-    enharmonicFlat: "Db",
+    natural: 'C#',
+    enharmonicSharp: '',
+    enharmonicFlat: 'Db',
   },
   {
-    natural: "D",
-    enharmonicSharp: "",
-    enharmonicFlat: "",
+    natural: 'D',
+    enharmonicSharp: '',
+    enharmonicFlat: '',
   },
   {
-    natural: "D#",
-    enharmonicSharp: "",
-    enharmonicFlat: "Eb",
+    natural: 'D#',
+    enharmonicSharp: '',
+    enharmonicFlat: 'Eb',
   },
   {
-    natural: "E",
-    enharmonicSharp: "",
-    enharmonicFlat: "Fb",
+    natural: 'E',
+    enharmonicSharp: '',
+    enharmonicFlat: 'Fb',
   },
   {
-    natural: "F",
-    enharmonicSharp: "E#",
-    enharmonicFlat: "",
+    natural: 'F',
+    enharmonicSharp: 'E#',
+    enharmonicFlat: '',
   },
   {
-    natural: "F#",
-    enharmonicSharp: "",
-    enharmonicFlat: "Gb",
+    natural: 'F#',
+    enharmonicSharp: '',
+    enharmonicFlat: 'Gb',
   },
   {
-    natural: "G",
-    enharmonicSharp: "",
-    enharmonicFlat: "",
+    natural: 'G',
+    enharmonicSharp: '',
+    enharmonicFlat: '',
   },
   {
-    natural: "G#",
-    enharmonicSharp: "",
-    enharmonicFlat: "Ab",
+    natural: 'G#',
+    enharmonicSharp: '',
+    enharmonicFlat: 'Ab',
   },
   {
-    natural: "A",
-    enharmonicSharp: "",
-    enharmonicFlat: "",
+    natural: 'A',
+    enharmonicSharp: '',
+    enharmonicFlat: '',
   },
   {
-    natural: "A#",
-    enharmonicSharp: "",
-    enharmonicFlat: "B",
+    natural: 'A#',
+    enharmonicSharp: '',
+    enharmonicFlat: 'B',
   },
   {
-    natural: "H",
-    enharmonicSharp: "",
-    enharmonicFlat: "Cb",
+    natural: 'H',
+    enharmonicSharp: '',
+    enharmonicFlat: 'Cb',
   },
 ];
 
-class Tonality {
+export class Tonality {
   getTonality() {
     return T;
   }
 
-  getTonalitySteps(tonicChord: string) {
-    const indexOfTonolity = T.findIndex(
-      (tonality) =>
-        tonicChord.includes(
-          tonality.natural
-        )
-      // tonality.natural.includes(tonicChord)
-    );
+  loopInRange(number: number) {
+    return number % T.length;
+  }
 
-    // T.filter((tonality, i, arr) => {});
+  getTonalitySteps(tonicChord: string) {
+    const indexOfTonolity = T.findIndex((tonality) =>
+      tonicChord.includes(tonality.natural),
+    );
 
     function* generateDurStep() {
       yield T[indexOfTonolity];
-      yield T[indexOfTonolity + 2] ||
-        T[
-          T.length - indexOfTonolity + 2
-        ];
-      yield T[indexOfTonolity + 4] ||
-        T[
-          indexOfTonolity + 4 - T.length
-        ];
-      yield T[indexOfTonolity + 5] ||
-        T[
-          indexOfTonolity + 5 - T.length
-        ];
-      yield T[indexOfTonolity + 7] ||
-        T[
-          indexOfTonolity + 7 - T.length
-        ];
-      yield T[indexOfTonolity + 9] ||
-        T[
-          indexOfTonolity + 9 - T.length
-        ];
-      yield T[indexOfTonolity + 11] ||
-        T[
-          indexOfTonolity +
-            11 -
-            T.length
-        ];
+      yield T[indexOfTonolity + 2] || T[T.length - indexOfTonolity + 2];
+      yield T[indexOfTonolity + 4] || T[indexOfTonolity + 4 - T.length];
+      yield T[indexOfTonolity + 5] || T[indexOfTonolity + 5 - T.length];
+      yield T[indexOfTonolity + 7] || T[indexOfTonolity + 7 - T.length];
+      yield T[indexOfTonolity + 9] || T[indexOfTonolity + 9 - T.length];
+      yield T[indexOfTonolity + 11] || T[indexOfTonolity + 11 - T.length];
     }
 
     function* generateMollStep() {
       yield T[indexOfTonolity];
-      yield T[indexOfTonolity + 2] ||
-        T[
-          T.length - indexOfTonolity + 2
-        ];
-      yield T[indexOfTonolity + 3] ||
-        T[
-          indexOfTonolity + 3 - T.length
-        ];
-      yield T[indexOfTonolity + 5] ||
-        T[
-          indexOfTonolity + 5 - T.length
-        ];
-      yield T[indexOfTonolity + 7] ||
-        T[
-          indexOfTonolity + 7 - T.length
-        ];
-      yield T[indexOfTonolity + 8] ||
-        T[
-          indexOfTonolity + 8 - T.length
-        ];
-      yield T[indexOfTonolity + 11] ||
-        T[
-          indexOfTonolity +
-            11 -
-            T.length
-        ];
+      yield T[indexOfTonolity + 2] || T[T.length - indexOfTonolity + 2];
+      yield T[indexOfTonolity + 3] || T[indexOfTonolity + 3 - T.length];
+      yield T[indexOfTonolity + 5] || T[indexOfTonolity + 5 - T.length];
+      yield T[indexOfTonolity + 7] || T[indexOfTonolity + 7 - T.length];
+      yield T[indexOfTonolity + 8] || T[indexOfTonolity + 8 - T.length];
+      yield T[indexOfTonolity + 11] || T[indexOfTonolity + 11 - T.length];
     }
 
     const major = generateDurStep();
     const minor = generateMollStep();
-    const harmony = tonicChord.includes(
-      "m"
-    )
-      ? minor
-      : major;
+    const harmony = tonicChord.includes('m') ? minor : major;
     let steps = [];
 
     for (let value of harmony) {
       steps.push(value);
     }
-    // debugger
-    console.log(
-      // harmony,
-      // T,
-      tonicChord,
-      indexOfTonolity,
-      T.length,
-      T.length - indexOfTonolity
-    );
+
     return steps;
+  }
+
+  fifthCircle() {
+    const ton = this.getTonality();
+
+    let count = 0;
+    let result = [];
+    while (count < 12) {
+      count === 0 && result.push(ton[0]);
+      count && result.push(ton[this.loopInRange(count * 7)]);
+      count++;
+    }
+
+    const test = result.map((item, index, arr) => {
+      if (index < arr.length / 2) {
+        return item.natural || item.enharmonicSharp;
+      } else {
+        return item.enharmonicFlat || item.natural;
+      }
+    });
+
+    return test;
   }
 }
 
-console.log(
-  new Tonality().getTonalitySteps("A")
-);
-
-const CHORDS_LIST = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-];
-const ALTERATION_SIGNS = ["#", "b"];
+const CHORDS_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+const ALTERATION_SIGNS = ['#', 'b'];
 export let TONALITIES: string[] = [];
 CHORDS_LIST.forEach((c) => {
   TONALITIES.push(c);
-  ALTERATION_SIGNS.forEach((a) =>
-    TONALITIES.push(c + a)
-  );
+  ALTERATION_SIGNS.forEach((a) => TONALITIES.push(c + a));
 });
 
 export const sectionTypes = [
-  "вступление:",
-  "куплет:",
-  "припев:",
-  "проигрыш:",
-  "бридж:",
-  "конец:",
+  'вступление:',
+  'куплет:',
+  'припев:',
+  'проигрыш:',
+  'бридж:',
+  'конец:',
 ];
 
-export const isTitle = (
-  row: string
-) => {
-  return sectionTypes.some((m) =>
-    row.toLowerCase().includes(m)
-  );
+export const isTitle = (row: string) => {
+  return sectionTypes.some((m) => row.toLowerCase().includes(m));
 };
 
-export const isChords = (
-  row: string
-) => {
-  const chordsFromRow = row
-    ?.split(" ")
-    .filter((rowItem) => rowItem);
+export const isChords = (row: string) => {
+  const chordsFromRow = row?.split(' ').filter((rowItem) => rowItem);
 
-  const res = chordsFromRow?.every(
-    (chordInput) => {
-      return TONALITIES.some(
-        (chord) => {
-          return chordInput.includes(
-            chord
-          );
-        }
-      );
-    }
-  );
+  const res = chordsFromRow?.every((chordInput) => {
+    return TONALITIES.some((chord) => {
+      return chordInput.includes(chord);
+    });
+  });
 
   return res;
 };
