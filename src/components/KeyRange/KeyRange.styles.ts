1e3a2817b15f455b9d5a33cf3bfc975d1392954d
Boris <rozumniy.boris.work@gmail.com>
2021-12-10 14:40:44 +0200

    change the scroll style for the RangeContainer

--- src/components/KeyRange/KeyRange.styles.ts
+++ src/components/KeyRange/KeyRange.styles.ts
@@ -3,4 +3,13 @@ import styled from 'styled-components';
 export const RangeContainer = styled.div`
   display: flex;
   overflow: auto;
+
+  ::-webkit-scrollbar {
+    height: 4px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #efc;
+    border-radius: 3px;
+  }
 `;
