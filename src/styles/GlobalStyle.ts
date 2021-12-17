64609c71d6fb4c96c76e6ccb9c4e9558060f9bd7
Boris <rozumniy.boris.work@gmail.com>
2021-12-17 12:48:43 +0200

    styled the content-editable and the chord classes

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,4 +4,12 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: #ccc;
   }
+
+  .content-editable {
+    border: solid green 2px;
+  }
+
+  .chord {
+    color: red;
+  }
 `;
